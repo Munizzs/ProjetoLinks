9d788aca301d423a4a13787942fbb44b890b2336
Matheus Muniz <matheusmuniz0810@gmail.com>
2024-06-13 14:48:27 -0300

    Modo light criado, JavaScript implementado com suas funcionalidades, criação do switch e site responsivo com o @media

---
+++ script.js
@@ -0,0 +1,11 @@
+function toggleMode() {
+  const html = document.documentElement
+
+  /* if (html.classList.contains("light")) {
+    html.classList.remove("light")
+  } else {
+    html.classList.add("light")
+  } */
+
+    html.classList.toggle("light") /*Acontece a mesma coisa que a condição só que mais simples */
+}
